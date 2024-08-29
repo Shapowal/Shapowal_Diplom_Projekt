c5635ed9cb188e5fa7f19f7a0ebab9b9553cfb9e
Shapowal <154756495+Shapowal@users.noreply.github.com>
2024-08-29 13:19:24 +0300

    Update views.py
    проверка на вход / защита представлений для незарегестрированных или не выполнивших вход пользователей

--- pythonProjectd2/sklad1/views.py
+++ pythonProjectd2/sklad1/views.py
@@ -7,6 +7,17 @@ from django.views.generic import ListView
 from .forms import *
 from .models import *
 
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models import Max
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.views.generic import ListView
+from .forms import *
+from .models import *
+
+
 # Регистрация нового пользователя
 def register(request):
     """Регистрация нового пользователя и автоматический вход в систему"""
@@ -25,6 +36,7 @@ def register(request):
 
     role_choices = CustomUser.ROLE_CHOICES  # Получаем варианты ролей для выбора
     return render(request, 'autentification/register.html', {'form': form, 'role_choices': role_choices})
+
 
 # Вход пользователя в систему
 def login_view(request):
@@ -58,29 +70,37 @@ def home(request):
     """Отображает главную страницу."""
     return render(request, 'page_web/home.html')
 
+
 # Представления для страниц склада и производства
+@login_required
 def warehouse_view(request):
     """Отображает страницу склада."""
     return render(request, 'warehause_page/warehouse.html')
 
+@login_required
 def production_view(request):
     """Отображает страницу производства."""
     return render(request, 'warehause_page/production.html')
 
+@login_required
 def finished_goods_warehouse_view(request):
     """Отображает страницу склада готовой продукции."""
     return render(request, 'warehause_page/finished_goods_warehouse.html')
 
+@login_required
 def finished_goods_stock(request):
     """Отображает список партий готовой продукции с ненулевым количеством."""
     batches = Batch.objects.filter(quantity__gt=0, is_used=False)
     return render(request, 'warehause_page/finished_goods_stock_list.html', {'batches': batches})
 
+@login_required
 def production_main(request):
     """Отображает главную страницу производства."""
     return render(request, 'warehause_page/production_main.html')
 
+
 # Создание новой линии
+@login_required
 def create_line(request):
     """Обрабатывает создание новой производственной линии."""
     if request.method == 'POST':
@@ -95,7 +115,9 @@ def create_line(request):
         form = LineForm()
     return render(request, 'lines/create_line.html', {'form': form})
 
+
 # Список всех линий
+@login_required
 class LineListView(ListView):
     """Отображает список всех производственных линий."""
     model = Line
@@ -103,7 +125,9 @@ class LineListView(ListView):
     context_object_name = 'lines'
     ordering = ['number']
 
+
 # Добавление продукта на линию
+@login_required
 def add_product_to_line(request):
     """Обрабатывает добавление продукта на производственную линию."""
     if request.method == 'POST':
@@ -118,14 +142,18 @@ def add_product_to_line(request):
         form = ProductForm()
     return render(request, 'lines/add_product.html', {'form': form})
 
+
 # Список всех продуктов и линий
+@login_required
 def product_list(request):
     """Отображает список всех продуктов и линий."""
     products = Product.objects.all()
     lines = Line.objects.all()
     return render(request, 'lines/product_list.html', {'products': products, 'lines': lines})
 
+
 # Создание новой партии
+@login_required
 def create_batch(request):
     """Обрабатывает создание новой партии продукции."""
     if request.method == 'POST':
@@ -162,6 +190,7 @@ def create_batch(request):
 
 
 # Список всех партий с фильтрацией по дате
+@login_required
 def batch_list(request):
     """Отображает список партий продукции с возможностью фильтрации по дате и продукту."""
     start_date = request.GET.get('start_date')
@@ -182,27 +211,32 @@ def batch_list(request):
 
     return render(request, 'lines/batch_list.html', {'batches': batches, 'products': products})
 
+
 # Загрузка продуктов по линии
+@login_required
 def load_products(request):
     """Возвращает список продуктов для выбранной линии в формате JSON."""
     line_id = request.GET.get('line_id')
     products = Product.objects.filter(line_id=line_id).order_by('name')
     return JsonResponse({'products': list(products.values('id', 'name'))})
 
+
 # Добавление количества к партии
 # (Функция не реализована в текущем коде)
 
 # Представления для страниц со складами и остатками
+@login_required
 def warehouse(request):
     """Отображает страницу склада."""
     return render(request, 'warehause_page/warehouse.html')
 
+@login_required
 def view_stock(request):
     """Отображает список всех материалов на складе."""
     materials = Material.objects.all()
     return render(request, 'materials/view_stock.html', {'materials': materials})
 
-
+@login_required
 def view_finished_goods_stock(request):
     """Отображает список всех партий готовой продукции с ненулевым количеством."""
     batches = Batch.objects.filter(quantity__gt=0, is_used=False)
@@ -210,6 +244,7 @@ def view_finished_goods_stock(request):
 
 
 # Создание нового материала
+@login_required
 def create_material(request):
     """Обрабатывает создание нового материала."""
     if request.method == 'POST':
@@ -224,7 +259,9 @@ def create_material(request):
         form = MaterialForm()
     return render(request, 'materials/create_material.html', {'form': form})
 
+
 # Добавление материала к продукту
+@login_required
 def create_product_material(request):
     """Обрабатывает добавление материала к продукту."""
     if request.method == 'POST':
@@ -239,19 +276,25 @@ def create_product_material(request):
         form = ProductMaterialForm()
     return render(request, 'materials/create_product_material.html', {'form': form})
 
+
 # Список всех материалов
+@login_required
 def material_list(request):
     """Отображает список всех материалов."""
     materials = Material.objects.all()
     return render(request, 'materials/material_list.html', {'materials': materials})
 
+
 # Список всех связей продуктов и материалов
+@login_required
 def product_material_list(request):
     """Отображает список всех связей продуктов и материалов."""
     product_materials = ProductMaterial.objects.all()
     return render(request, 'materials/product_material_list.html', {'product_materials': product_materials})
 
+
 # Выпуск продукции и списание материалов
+@login_required
 def release_products(request):
     """Обрабатывает выпуск продукции и списание материалов со склада."""
     if request.method == 'POST':
@@ -306,7 +349,9 @@ def release_products(request):
 
     return render(request, 'materials/release_products.html', {'form': form})
 
+
 # Создание новой отгрузки
+@login_required
 def create_shipment(request):
     """Обрабатывает создание новой отгрузки продукции."""
     if request.method == 'POST':
@@ -345,7 +390,9 @@ def create_shipment(request):
     form.fields['batch'].queryset = FinishedGoodsStock.objects.filter(quantity__gt=0, is_used=False)
     return render(request, 'warehause_page/create_shipment.html', {'form': form})
 
+
 # Просмотр всех отгрузок
+@login_required
 def view_shipments(request):
     """Отображает список всех отгрузок с возможностью фильтрации по дате."""
     start_date = request.GET.get('start_date')
@@ -358,11 +405,13 @@ def view_shipments(request):
 
     return render(request, 'warehause_page/view_shipments.html', {'shipments': shipments})
 
+@login_required
 def check_incoming(request):
     """Отображает страницу проверки поступлений товаров на склад."""
     batches = Batch.objects.filter(quantity__gt=0, is_used=False)
     return render(request, 'warehause_page/check_incoming.html', {'batches': batches})
 
+@login_required
 def edit_finished_goods(request, batch_id):
     """Обрабатывает редактирование данных о готовой продукции."""
     batch = get_object_or_404(Batch, id=batch_id)
@@ -375,6 +424,7 @@ def edit_finished_goods(request, batch_id):
         form = BatchForm(instance=batch)
     return render(request, 'warehause_page/edit_finished_goods.html', {'form': form})
 
+@login_required
 def create_counterparty(request):
     """Обрабатывает создание нового контрагента."""
     if request.method == 'POST':
@@ -387,12 +437,15 @@ def create_counterparty(request):
 
     return render(request, 'warehause_page/create_counterparty.html', {'form': form})
 
+@login_required
 def counterparty_list(request):
     """Отображает список всех контрагентов."""
     counterparties = Counterparty.objects.all()
     return render(request, 'warehause_page/counterparty_list.html', {'counterparties': counterparties})
 
+
 # Представление для добавления остатка
+@login_required
 def add_stock(request):
     """Обрабатывает добавление нового остатка на склад."""
     if request.method == 'POST':
@@ -405,7 +458,9 @@ def add_stock(request):
 
     return render(request, 'materials/add_stock.html', {'form': form})
 
+
 # Представление для редактирования остатка
+@login_required
 def edit_stock(request, stock_id):
     """Обрабатывает редактирование остатка на складе."""
     stock = get_object_or_404(Stock, id=stock_id)
@@ -419,7 +474,9 @@ def edit_stock(request, stock_id):
 
     return render(request, 'materials/edit_stock.html', {'form': form})
 
+
 # Представление для просмотра и редактирования остатков
+@login_required
 def view_and_edit_stock(request):
     """Отображает список всех остатков и позволяет редактировать их."""
     if request.method == 'POST':
@@ -436,7 +493,9 @@ def view_and_edit_stock(request):
     stocks = Stock.objects.all()
     return render(request, 'materials/view_and_edit_stock.html', {'stocks': stocks, 'form': form})
 
+
 # Обновление функции для использования нового шаблона
+@login_required
 def finished_goods_stock_list(request):
     """Отображает список всех остатков готовой продукции."""
     stock = FinishedGoodsStock.objects.all()
